65412691e15b19d195af3a90618e9d4e2ff2cb13
Samuel Tong <STong212@gmail.com>
2024-04-16 23:16:49 +0100

    added uncommitted files

---
+++ src/templates/pageTemplate/PageTemplate.jsx
@@ -0,0 +1,20 @@
+import './PageTemplate.scss'
+
+// Organisms
+import Navbar from '../../organisms/navbar/Navbar'
+
+const PageTemplate = ({ component }) => {
+
+    return (
+        <div className="PageTemplate">
+
+            <Navbar className="PageTemplate_Navbar" />
+
+            {
+                component
+            }
+        </div>
+    )
+}
+
+export default PageTemplate
